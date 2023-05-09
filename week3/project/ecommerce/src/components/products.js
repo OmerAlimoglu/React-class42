47c8168e6e4fc5d8ff3ccc1a83ba5a8f5fc056a7
Abdullah Omer ALIMOGLU <aoalimoglu@gmail.com>
2023-05-09 08:02:42 +0200

    created a custom hook in order to fetch data and also a context

--- week3/project/ecommerce/src/components/products.js
+++ week3/project/ecommerce/src/components/products.js
@@ -1,53 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext } from "react";
 import "./Product.css";
 import { Link } from "react-router-dom";
+import { FavoritesContext } from "./FavoritesContext";
+import useFetch from "../useFetch.js";
+import { ReactComponent as HeartRegular } from "../assets/heart-regular.svg";
+import { ReactComponent as HeartSolid } from "../assets/heart-solid.svg";
 
 const Products = ({ categoryName }) => {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const { favorites, toggleFavorite } = useContext(FavoritesContext);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const url = categoryName
-          ? `https://fakestoreapi.com/products/category/${categoryName}`
-          : `https://fakestoreapi.com/products`;
-        const response = await fetch(url);
-        const products = await response.json();
-        setProducts(products);
-        setError(null);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchProducts();
-  }, [categoryName]);
+  const url = categoryName
+    ? `https://fakestoreapi.com/products/category/${categoryName}`
+    : `https://fakestoreapi.com/products`;
+
+  console.log("Products component rendering...");
+
+  const { data: products, error, loading } = useFetch(url);
+  console.log(products);
+
+  const handleFavoriteToggle = (productId) => {
+    toggleFavorite(productId);
+  };
 
   return (
     <div className="products">
       {loading ? (
-        <div className="loading">Loading...</div>
+        <div className="loading">{loading}</div>
       ) : error ? (
-        <p>Something went wrong: {error}</p>
-      ) : categoryName && products.length === 0 ? (
+        <p>Something went wrong: {error.toString()}</p>
+      ) : categoryName && products && products.length === 0 ? (
         <p>No products found for {categoryName}</p>
       ) : (
         products.map(({ id, image, title }) => (
           <div key={id} className="product">
-            {categoryName ? (
-              <Link to={`/product/${id}`}>
-                <img src={image} alt={title} />
-                <h3>{title}</h3>
-              </Link>
-            ) : (
-              <Link to={`/product/${id}`}>
-                <img src={image} alt={title} />
-                <h3>{title}</h3>
-              </Link>
-            )}
+            <Link to={`/product/${id}`}>
+              <img src={image} alt={title} />
+              <h3>{title}</h3>
+            </Link>
+            <button onClick={() => handleFavoriteToggle(id)}>
+              {favorites && favorites.includes(id) ? (
+                <HeartSolid />
+              ) : (
+                <HeartRegular />
+              )}
+            </button>
           </div>
         ))
       )}
---
+++ week3/project/ecommerce/src/useFetch.js
@@ -0,0 +1,82 @@
+// I used this site to create a custom hook in order to fetch API data
+// but I got errors and managed to fix it adding cleanup function
+//that cancels the request if the component is unmounted.. (thanks chatGPT :) )
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [cancelToken, setCancelToken] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+
+    const source = axios.CancelToken.source();
+    setCancelToken(source);
+
+    axios
+      .get(url, { cancelToken: source.token })
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled: Component unmounted");
+        } else {
+          setError(error);
+          setLoading(false);
+        }
+      });
+
+    return () => {
+      if (cancelToken) {
+        cancelToken.cancel("Component unmounted");
+      }
+    };
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
+
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+
+// function useFetch(url) {
+//   console.log("useFetch called with url: ", url);
+//   const [data, setData] = useState(null);
+//   const [isLoading, setIsLoading] = useState(null);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     console.log("Fetching data...");
+//     setIsLoading("Loading...");
+//     setData(null);
+//     setError(null);
+//     const source = axios.CancelToken.source();
+//     axios
+//       .get(url, { cancelToken: source.token })
+//       .then((res) => {
+//         console.log(res.data);
+//         setIsLoading(false);
+//         setData(res.data.content || res.content);
+//       })
+//       .catch((err) => {
+//         setIsLoading(false);
+//         setError("An error occurred...");
+//         console.log("Error fetching data:", err);
+//       });
+//     return () => {
+//       source.cancel();
+//     };
+//   }, [url]);
+
+//   return { data, isLoading, error };
+// }
+
+// export default useFetch;
